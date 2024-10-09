1a92287761e2b87f86a1d15825587bde5bef526d
giovannipro <g.profet@gmail.com>
2024-10-09 09:47:48 +0200

    move the statistics in a new file and start implementing the zoom

--- assets/script/utilities.js
+++ assets/script/utilities.js
@@ -62,3 +62,150 @@ function short_url(url,max){
 	}
 	return shortUrl
 }
+
+function load_statistics(data){
+
+	const max_link_char = 50
+
+	const container_a = document.getElementById('statistics_a')
+	const container_b = document.getElementById('statistics_b')
+	const container_c = document.getElementById('statistics_c')
+
+	const searchItems = data.filter(item => item.action === 'NEW_SEARCH' || item.action === 'NEW_SEARCH_SAME_ENGINE' || item.action === 'REFINE_SEARCH');
+	const searchDuration = searchItems.reduce((sum, item) => sum + item.duration, 0);
+	const avgSearchDuration = searchDuration / searchItems.length; 
+	const minSearchDuration = Math.min(...searchItems.map(item => item.duration));
+	const maxSearchDuration = Math.max(...searchItems.map(item => item.duration));
+
+	const pageItems = data.filter(item => item.action === 'NEW_RESULT' || item.action === 'SAME_DOMAIN_RESULT' || item.action == "SEEN_DOMAIN_RESULT");
+	const pageDuration = pageItems.reduce((sum, item) => sum + item.duration, 0);
+	const avgPageDuration = pageDuration / pageItems.length; 
+	const minPageDuration = Math.min(...pageItems.map(item => item.duration));
+	const maxPageDuration = Math.max(...pageItems.map(item => item.duration));
+
+	const newQueries = data.filter(item => item.action === 'NEW_SEARCH' || item.action === 'NEW_SEARCH_SAME_ENGINE').length
+	const reusedQueries = data.filter(item => item.action === 'SAME_SEARCH' || item.action === 'SEEN_SEARCH').length
+	const revisedQueries = data.filter(item => item.action === 'REFINE_SEARCH').length
+
+
+	const newDomains = data.filter(item => item.action === 'NEW_RESULT').length
+	const revisitedDomains = data.filter(item => item.action === 'SEEN_DOMAIN_RESULT').length
+	const pages = data.filter(item => item.action === 'NEW_RESULT' || item.action === 'SAME_DOMAIN_RESULT' || item.action === 'SEEN_DOMAIN_RESULT').length
+
+	const queries = searchItems.map(item => item.url)
+		.filter(item => item.indexOf('http') >= 0)
+	const unique_queries = getUniqueValues(queries) 
+
+	// const websites = pageItems.map(item => item.url)
+	unique_web = []
+	pageItems.forEach(item => {
+		url = item.url
+		url_ = url.split("/")[2]
+		unique_web.push(url_)
+	})
+	const unique_websites = getUniqueValues(unique_web) 
+
+	uniq_engine = []
+	searchItems.forEach(item => {
+		url = item.url
+		url_ = url.split("search?")[0]
+		uniq_engine.push(url_)
+	})
+	const unique_searchEngines = getUniqueValues(uniq_engine) 
+
+	let output_a = ''
+	let output_b = ''
+	let output_c = ''
+
+	output_a += '<table>'
+	output_a += '<tr><td><strong>Duration</strong></td></tr>'
+	output_a += '<tr><td>Search (shortest)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(minSearchDuration) + '</td></tr>'
+	output_a += '<tr><td>Search (average)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(avgSearchDuration) + '</td></tr>'
+	output_a += '<tr><td>Search (longest)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(maxSearchDuration) + '</td></tr>' 
+	output_a += '<tr><td>Search (total)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(searchDuration) + '</td></tr>' // '<td>' + parseInt(searchDuration) + ' seconds / ' + convertSecondsToMinutes(searchDuration) + ' minutes</td></tr>'
+	output_a += '<tr><td>&nbsp;</td></tr>'
+
+	output_a += '<tr><td>Pages (shortest)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(minPageDuration) + '</td></tr>'
+	output_a += '<tr><td>Pages (average)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(avgPageDuration) + '</td></tr>' 
+	output_a += '<tr><td>Pages (longest)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(maxPageDuration) + '</td></tr>' 
+	output_a += '<tr><td>Pages (total)</td>'
+	output_a += '<td>' + convertSecondsToMinutes(pageDuration) + '</td></tr>' // '<td>' + parseInt(pageDuration) + ' seconds / ' + convertSecondsToMinutes(pageDuration) + ' minutes</td></tr>'
+	output_a += '<tr><td>&nbsp;</td></tr>'
+
+	output_a += '<tr><td>Total</td>'
+	output_a += '<td>' + convertSecondsToMinutes(pageDuration + searchDuration) + '</td></tr>' // '<td>' + parseInt(pageDuration + searchDuration) + ' seconds / ' + convertSecondsToMinutes(pageDuration + searchDuration) + ' minutes</td></tr>'
+	output_a += '</table>'
+
+	output_b += '<table>'
+	output_b += '<tr><td><strong>Search</strong></td></tr>'
+	output_b += '<tr><td>New queries</td>'
+	output_b += '<td>' + newQueries + '</td></tr>'
+	output_b += '<tr><td>Reused queries</td>'
+	output_b += '<td>' + reusedQueries + '</td></tr>'
+	output_b += '<tr><td>Reprised queries</td>'
+	output_b += '<td>' + revisedQueries + '</td></tr>'
+	output_b += '<tr><td>Total queries</td>'
+	output_b += '<td>' + (newQueries + reusedQueries + revisedQueries) + '</td></tr>'
+	output_b += '</table>'
+
+	output_c += '<table>'
+	output_c += '<tr><td><strong>Websites</strong></td></tr>'
+	output_c += '<tr><td>New websites</td>'
+	output_c += '<td>' + newDomains + '</td></tr>'
+	output_c += '<tr><td>Revisited websites</td>'
+	output_c += '<td>' + revisitedDomains + '</td></tr>'
+	output_c += '<tr><td>Total websites visited</td>'
+	output_c += '<td>' + (newDomains + revisitedDomains) + '</td></tr>'
+	output_c += '<tr><td>Total pages visited</td>'
+	output_c += '<td>' + pages + '</td></tr>'
+	output_c += '</table>'
+
+	output_b += '<table style="margin-top: 1.5rem;">'
+	output_b += '<tr><td><strong>Queries</strong> (chronological order)</td></tr>'
+	unique_queries.forEach(item => {
+		the_query = short_url(item,60)
+		if (item.split('?q=')[1]){
+			the_query_a = item.split('?q=')[1]
+			the_query_b = the_query_a.split('=')[0]
+			the_query_c = the_query_b.split('&')[0]
+			the_query = the_query_c.replace(/\+/g,' ')
+		}
+		else if (item.split('?safe=')[1]){
+			the_query_a = item.split('?safe=')[1]
+			the_query_b = the_query_a.split('=')[0]
+			the_query_c = the_query_b.split('&')[0]
+			the_query = the_query_c.replace(/\+/g,' ')
+		}
+		output_b += '<tr><td><a href="' + item + '" target="_blank">' + short_url(the_query,max_link_char) + '</a></td></tr>'
+	})
+	output_b += '</table>'
+
+	output_b += '<table style="margin-top: 1.5rem;">'
+	output_b += '<tr><td><strong>Search engines</strong></td></tr>'
+	unique_searchEngines.forEach(item => {
+		output_b += '<tr><td><a href="' + item + '" target="_blank">' + short_url(item,max_link_char) + '</a></td></tr>'
+	})
+	output_b += '</table>'
+
+	output_c += '<table style="margin-top: 1.5rem;">'
+	output_c += '<tr><td><strong>Websites</strong> (chronological order)</td></tr>'
+	unique_websites.forEach(item => {
+		const web_a = item.replace('https://','')
+		const web_b = web_a.replace('www.','')
+		output_c += '<tr><td><a href="' + item + '" target="_blank">' + short_url(web_b,max_link_char) + '</a></td><tr>' // 
+	})
+	output_c += '</tr>'
+	output_c += '</table>'
+
+	container_a.innerHTML = output_a
+	container_b.innerHTML = output_b
+	container_c.innerHTML = output_c
+
+}
